43f5a100d0b61c98d4c8d187d4ea0e4a2cc9a29b
KobiVG <kobevangestel@outlook.com>
2024-12-14 12:38:05 +0100

    initialized db and ensured connection

---
+++ index.js
@@ -0,0 +1,27 @@
+// Importeren van de express module in node_modules
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const Database = require('./classes/database.js');
+
+// Aanmaken van een express app
+const app = express();
+
+// Enable CORS
+app.use(cors({
+    origin: 'http://localhost:8080', // Allow requests from this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+}));
+
+// Middleware om JSON-requests te parsen
+app.use(bodyParser.json());
+
+// Endpoints
+
+
+
+// Starten van de server en op welke port de server moet luistere
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
